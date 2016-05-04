617c36a0604a76e6e36f6928f542b305bb6f61b2
junghomin <plus.jungho@gmail.com>
2016-05-05 08:13:30 +0900

    initial setup

---
+++ src/main/java/jungho/demo/JunghoDemoApplication.java
@@ -0,0 +1,12 @@
+package jungho.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunghoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunghoDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/jungho/demo/JunghoDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package jungho.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JunghoDemoApplication.class)
+@WebAppConfiguration
+public class JunghoDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
